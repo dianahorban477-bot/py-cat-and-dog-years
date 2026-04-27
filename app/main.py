da31549af8089704d9211d3a2819bf9a860168d5
dianahorban <dianahorban477@gmail.com>
2026-04-27 18:08:43 +0200

    The solution

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ def get_human_age(cat_age: int, dog_age: int) -> list:
             return 0
         age -= 15
         if age <= 9:
-            return 1
+            return 2
         age -= 9
-        return 1 + 1
+        return 2 + age // step
     return [convert(cat_age, 4), convert(dog_age, 5)]
